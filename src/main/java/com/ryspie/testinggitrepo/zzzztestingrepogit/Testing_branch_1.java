8ca380a656572c7af0c50d58a06547a6d83ffc86
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 18:44:00 +0200

    First change on branch main)

---
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_1.java
@@ -0,0 +1,10 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+import java.util.List;
+
+class Testing_branch_1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        numbers.forEach(System.out::println);
+    }
+}
