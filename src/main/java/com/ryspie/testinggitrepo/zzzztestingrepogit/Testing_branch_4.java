ccc7b7f09e1e65efc16c243126aeea9c2dbc9a85
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 18:54:51 +0200

    Third change on main

---
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_4.java
@@ -0,0 +1,10 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+import java.util.List;
+
+class Testing_branch_4 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(100, 200, 300, 400, 500);
+        numbers.forEach(System.out::println);
+    }
+}
