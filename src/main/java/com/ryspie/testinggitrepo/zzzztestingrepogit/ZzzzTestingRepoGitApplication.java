6eb8cde1a8c62870eab49b210a087a4c47997ec5
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 18:25:17 +0200

    Initial commit

---
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/ZzzzTestingRepoGitApplication.java
@@ -0,0 +1,13 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzzzTestingRepoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzzzTestingRepoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryspie/testinggitrepo/zzzztestingrepogit/ZzzzTestingRepoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzzzTestingRepoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
