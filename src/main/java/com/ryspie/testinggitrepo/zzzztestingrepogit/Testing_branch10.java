a1ef019a630f20477733f4e650019f87134b1ea2
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 20:01:20 +0200

    BUGFIX 2

---
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch10.java
@@ -0,0 +1,7 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+class Testing_branch10 {
+    public static void main(String[] args) {
+        System.out.println("Hello bugfix 2");
+    }
+}
