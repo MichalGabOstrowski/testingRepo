3893244e202e0ab1da6fe0fe378c5333a771511e
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 19:28:09 +0200

    Chenge features for SPECIFIC COMMIT

--- src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_1.java
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_1.java
@@ -6,5 +6,7 @@ class Testing_branch_1 {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
         numbers.forEach(System.out::println);
+
+        System.out.println("--------------");
     }
 }
