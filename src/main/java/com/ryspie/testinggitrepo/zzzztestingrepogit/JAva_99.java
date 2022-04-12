4f6088fe5e1a3a2100ec94627016c341c839b28f
Ryszard P <Riczm0ndes@gmail.com>
2022-04-12 20:51:20 +0200

    Add class 99

---
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/JAva_99.java
@@ -0,0 +1,7 @@
+package com.ryspie.testinggitrepo.zzzztestingrepogit;
+
+class JAva_99 {
+    public static void main(String[] args) {
+        System.out.println("Add new class 99");
+    }
+}
--- src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_4.java
+++ src/main/java/com/ryspie/testinggitrepo/zzzztestingrepogit/Testing_branch_4.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 class Testing_branch_4 {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(100, 200, 300, 400, 500);
+        List<Integer> numbers = List.of(1000, 2000, 3000, 4000, 5000);
         numbers.forEach(System.out::println);
     }
 }
